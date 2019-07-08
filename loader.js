84f4d057a5feb1da05c2e750a484c8561bbca232
Steve Hillman <hillman@sfu.ca>
2019-07-08 10:49:54 -0700

    Refactored to support multiple import files
    The loader can now handle 3 types of imports
     - reg_names.xml
     - hap812.xml
     - instructors.xml
    
    The loader auto-senses which import file was specified based
    on the XML content present. All SoR data is still loaded into a
    single table

--- loader.js
+++ loader.js
@@ -11,9 +11,14 @@ var inspect = require('eyes').inspector({maxLength: false})
 var importFile = process.argv[2]
 
 var students
+var depts
+var courses
+var employees = new Map
+var instructors = new Map
+var employees_in_db
+var instructors_in_db
+var students_in_db
 var sfuids_present = []
-var students_in_db = new Map
-var students_loaded = false
 var updates = {
                 updated: 0,
                 reactivated: 0,
@@ -21,12 +26,6 @@ var updates = {
                 removed: 0
             }
 
-var async_processed=0
-db.queue.on('active', () => {
-    if (++async_processed % 1000 === 0 || db.queue.size === 0) {
-        console.log(`Working on item ${async_processed}.  Size: ${db.queue.size}  Pending: ${db.queue.pending}`);
-    }
-});
 
 // Suck in the import file
 var stripPrefix = xml2js.processors.stripPrefix
@@ -40,12 +39,10 @@ var parser = new xml2js.Parser( {
                 tagNameProcessors: [stripPrefix] 
             })
 
-fs.readFile(importFile, async function(err, data) {
+fs.readFile(importFile, 'utf8', async function(err, data) {
     // Load existing active students from the DB
-    console.log("Updates: " + inspect(updates))
-    await loadFromDb()
     console.log("Loading XML extract from " + importFile)
-    parser.parseString(data, async function (err, extract) {
+    parser.parseString(data.replace(/&/g,'&amp;'), async function (err, extract) {
         if (err !== null) {
             console.log("Error parsing input. Can't continue. " + err);
         }
@@ -53,16 +50,32 @@ fs.readFile(importFile, async function(err, data) {
             var timestamp = extract.$.Timestamp
             // TODO: Compare timestamp against last processed one in DB and abort if older/same
             console.log("Extract time stamp: " + timestamp)
-            students = extract.student
-            await processImport()
+            if (extract.hasOwnProperty('student')) {
+                students = extract.student
+                students_in_db = await loadFromDb('SIMS')
+                await processStudentImport()
+            }
+            else if (extract.hasOwnProperty('department')) {
+                depts = extract.department
+                employees_in_db = await loadFromDb('HAP')
+                await processEmployeeImport()
+            }
+            else if (extract.hasOwnProperty('course')) {
+                courses = extract.course
+                //inspect(extract.course)
+                instructors_in_db = await (loadFromDb('SIMSINSTRUCT'))
+                await processInstructorImport()
+            }
+            else {
+                console.log("Extracted data unrecognized")
+            }
         }
-        await db.queue.onIdle()
-            console.log('Done')
-            console.log('Students with updates:      ' + updates.updated)
-            console.log('Students reactivated:       ' + updates.reactivated)
-            console.log('New students added:         ' + updates.inserted)
-            console.log('Students removed from feed: ' + updates.removed)
-            console.log(inspect(updates))
+        // await db.queue.onIdle()
+        console.log('Done')
+        console.log('Users with updates:      ' + updates.updated)
+        console.log('Users reactivated:       ' + updates.reactivated)
+        console.log('New Users added:         ' + updates.inserted)
+        console.log('Users removed from feed: ' + updates.removed)
     });
 });
 
@@ -70,7 +83,7 @@ fs.readFile(importFile, async function(err, data) {
 // This primarily just converts certain single-element values that could be arrays 
 // into arrays to ensure consistency
 //  
-function normalize(student) {
+function normalizeStudent(student) {
     if ( typeof student.reginfo === 'undefined' ) { return }
     if ( typeof student.reginfo.affiliation !== 'undefined' && !Array.isArray(student.reginfo.affiliation)) {
         student.reginfo.affiliation = [student.reginfo.affiliation]
@@ -84,8 +97,8 @@ function normalize(student) {
 }
 
 // Generate an MD5 hash of the JSONified student object and store it in the object
-function genhash(student) {
-    student.hash = hash('md5').update(JSON.stringify(student)).digest('base64')
+function genhash(person) {
+    person.hash = hash('md5').update(JSON.stringify(person)).digest('base64')
 }
 
 /* After the import file has been loaded, process it
@@ -96,17 +109,18 @@ function genhash(student) {
  * - Update or add changed/new students in DB
  * - Mark, as inactive, students who are no longer in the SoR feed
  */
-async function processImport() {
+async function processStudentImport() {
     students.forEach((student) => {
-        normalize(student)
+        normalizeStudent(student)
         genhash(student)
         sfuids_present.push(student.sfuid)
     })
     console.log("Active students loaded from XML: " + sfuids_present.length)
     
     students.forEach(async (student) => {
-        if (!students_in_db.has(student.sfuid) || students_in_db.get(student.sfuid) !== student.hash) {
-            await updateDbForStudent(student)
+        let isUpdate = students_in_db.has(student.sfuid)
+        if (!isUpdate || students_in_db.get(student.sfuid) !== student.hash) {
+            await updateDbForPerson(student, 'SIMS', isUpdate)
         }
     })
     if (students_in_db.size > 0) {
@@ -115,7 +129,7 @@ async function processImport() {
             if (!sfuids_present.includes(sfuid)) {
                 // Student is no longer in SoR feed. Set to inactive
                 try {
-                    var rows = await db.updateSorObject({sfuid: sfuid},{status:'inactive'})        
+                    var rows = await db.updateSorObject({sfuid: sfuid, source: 'SIMS'},{status:'inactive'})        
                     if (rows.length) {
                             updates.removed++
                     }
@@ -129,51 +143,222 @@ async function processImport() {
     }
 }
 
-async function loadFromDb() {  
+// Process the Employee import data. The import comes to us as an array of departments,
+// each with an array of employees. We need an array of employees each with an array of
+// jobs/departments. Do the conversion and then compare the resulting employee 
+// objects against the DB
+
+async function processEmployeeImport() {
+    depts.forEach((dept) => {
+        let empls = dept.employees
+        let role = 'employee'
+        if (empls.hasOwnProperty('applicant')) {
+            persons = empls.applicant
+            role = 'applicant'
+        }
+        else if (empls.hasOwnProperty('emp')) {
+            persons = empls.emp
+        }
+        else {
+            console.log("Unrecognized department employee type")
+            inspect(empls)
+            return
+        }
+        if (!Array.isArray(persons)) {
+            persons = [persons]
+        }
+        persons.forEach((person) => {
+            //console.log("Processing " + person.sfuid)
+            person.role = role
+            if (typeof person.job !== 'undefined') {
+                if (!Array.isArray(person.job)) {
+                    person.job = [person.job]
+                }
+                person.job.forEach((job) => {
+                    if ( typeof dept.deptcode !== 'undefined') {
+                        job.deptcode = dept.deptcode
+                    }
+                    if ( typeof dept.deptname !== 'undefined') {
+                        job.deptname = dept.deptname
+                    }
+                })
+            }
+            if (!employees.has(person.sfuid)) {
+                employees.set(person.sfuid,person)
+            }
+            else {
+                console.log("Adding job to " + person.sfuid)
+                let newperson = employees.get(person.sfuid)
+                newperson.job.push(...person.job)
+                employees.set(person.sfuid,newperson)
+                inspect(newperson)
+            }
+        })
+    })
+    employees.forEach(async (person) => {
+        genhash(person)
+        let isUpdate = employees_in_db.has(person.sfuid)
+        if (!isUpdate || employees_in_db.get(person.sfuid) !== person.hash) {
+            await updateDbForPerson(person, 'HAP', isUpdate)
+        }
+    })
+
+    if (employees_in_db.size > 0) {
+        for (const sfuid of employees_in_db.keys()) {
+            //console.log("present: " + sfuids_present.includes(sfuid))
+            if (!employees.has(sfuid)) {
+                // Employee is no longer in SoR feed. Set to inactive
+                try {
+                    var rows = await db.updateSorObject({sfuid: sfuid, source: 'HAP'},{status:'inactive'})        
+                    if (rows.length) {
+                            updates.removed++
+                    }
+                    else { console.log("what the..?")}
+                } catch(err) {
+                    console.log("Error updating status for employee: " + sfuid)
+                    console.log(err)
+                }
+            }
+        }
+    }
+}
+
+// Process the Instructors import data. The import comes to us as an array of courses,
+// each with an array of sections with an array of instructors. 
+// We need an array of instructors each with an array of sections. 
+// Do the conversion and then compare the resulting instructor objects against the DB
+async function processInstructorImport() {
+    courses.forEach((course) => {
+        if ( typeof course.classsections !== 'undefined' 
+          && typeof course.classsections.associated !== 'undefined') {
+            let assocs = course.classsections.associated
+            if (!Array.isArray(assocs)) {
+                assocs = [assocs]
+            }
+            assocs.forEach((assoc) => {
+                let sections = assoc.component
+                if (typeof sections === 'undefined') {
+                    // placeholder course - no defined sections or instructors yet
+                    return
+                }
+                if (!Array.isArray(sections)) {
+                    sections = [sections]
+                }
+                sections.forEach((section) => {
+                    if (typeof section.instructor !== 'undefined') {
+                        instructs = section.instructor
+                        if (!Array.isArray(instructs)) {
+                            instructs = [instructs]
+                        }
+                        instructs.forEach((instruct) => {
+                            // Sanity check on the SFUID field
+                            if (instruct.id.length < 5 || instruct.id < 9999) {
+                                return
+                            }
+                            instructor = {}
+                            instructor.sfuid = instruct.id
+                            instructor.sections = 
+                                [
+                                    {
+                                        term:     course.term,
+                                        name:     course.crsename,
+                                        num:      course.crsenum,
+                                        title:    course.crsetitle,
+                                        code:     section.$.code,
+                                        section:  section.sect,
+                                        type:     section.classtype,
+                                        status:   section.classstat,
+                                        rolecode: instruct.rolecode
+                                    }
+                                ]
+                            if (instructors.has(instructor.sfuid)) {
+                                let newinstructor = instructors.get(instructor.sfuid)
+                                newinstructor.sections.push(...instructor.sections)
+                                instructors.set(instructor.sfuid,newinstructor)
+                            }
+                            else {
+                                instructors.set(instructor.sfuid,instructor)
+                            }
+                        })
+                    }
+                })
+            })
+        } 
+    })
+    instructors.forEach(async (person) => {
+        genhash(person)
+        let isUpdate = instructors_in_db.has(person.sfuid)
+        if (!isUpdate || instructors_in_db.get(person.sfuid) !== person.hash) {
+            await updateDbForPerson(person, 'SIMSINSTRUCT', isUpdate)
+        }
+    })
+
+    if (instructors_in_db.size > 0) {
+        for (const sfuid of instructors_in_db.keys()) {
+            //console.log("present: " + sfuids_present.includes(sfuid))
+            if (!instructors.has(sfuid)) {
+                // Employee is no longer in SoR feed. Set to inactive
+                try {
+                    var rows = await db.updateSorObject({sfuid: sfuid, source: 'SIMSINSTRUCT'},{status:'inactive'})        
+                    if (rows.length) {
+                            updates.removed++
+                    }
+                    else { console.log("what the..?")}
+                } catch(err) {
+                    console.log("Error updating status for employee: " + sfuid)
+                    console.log(err)
+                }
+            }
+        }
+    }
+}
+
+async function loadFromDb(source) {  
+    var users_in_db = new Map
     try {
-        var rows = await db.getSorObjects(['sfuid','hash'],{status:'active'})
+        var rows = await db.getSorObjects(['sfuid','hash'],{status:'active',source: source})
         if (rows != null) {
             rows.forEach((row) => {
-                students_in_db.set(row.sfuid,row.hash)
+                users_in_db.set(row.sfuid,row.hash)
             })
-            students_loaded = true
-            console.log("Active students loaded from DB: " + students_in_db.size)
+            console.log("Active users loaded from DB for " + source + ": " + users_in_db.size)
         }
     } catch(err) {
-        console.log("Error loading students from DB. Can't continue!")
+        console.log("Error loading users from DB. Can't continue!")
         console.log(err)
         throw new Error("Something went badly wrong!");
         process.exit(1)
     }
+    return users_in_db
 }
 
-async function updateDbForStudent(student) {
+async function updateDbForPerson(person,source,isUpdate) {
     var rows
     var update_type = "updated"
     try {
-        if (students_in_db.has(student.sfuid)) {
+        if (isUpdate) {
             // Update an existing active student's record
             rows = await db.updateSorObject(
-                            {sfuid: student.sfuid},
+                            {sfuid: person.sfuid, source: source},
                             {
-                                hash: student.hash,
-                                lastname: student.lastname,
-                                firstnames: student.firstnames,
-                                userdata: JSON.stringify(student)
+                                hash: person.hash,
+                                lastname: person.lastname,
+                                firstnames: person.firstnames,
+                                userdata: JSON.stringify(person)
                             })
         }
         else {
             // Check whether the student exists in the DB at all yet
-            rows = await db.getSorObjects('id',{sfuid: student.sfuid})  
+            rows = await db.getSorObjects('id',{sfuid: person.sfuid, source: source})  
             if ( rows.length > 0) {
                 update_type = "reactivated"
                 // Student is in the DB but inactive. Update
-                    rows = await db.updateSorObject({sfuid: student.sfuid},{
+                    rows = await db.updateSorObject({sfuid: person.sfuid, source: source},{
                                     status: 'active',
-                                    hash: student.hash,
-                                    lastname: student.lastname,
-                                    firstnames: student.firstnames,
-                                    userdata: JSON.stringify(student)
+                                    hash: person.hash,
+                                    lastname: person.lastname,
+                                    firstnames: person.firstnames,
+                                    userdata: JSON.stringify(person)
                                 })
                 // TODO: If there are any other actions to kick off when a student re-appears, do it here
             }
@@ -181,13 +366,13 @@ async function updateDbForStudent(student) {
                 // Student not in DB. Insert
                 update_type = "inserted"
                 rows = await db.addSorObject({
-                        sfuid: student.sfuid,
+                        sfuid: person.sfuid,
                         status: 'active',
-                        hash: student.hash,
-                        lastname: student.lastname,
-                        firstnames: student.firstnames,
-                        source: 'SIMS',
-                        userdata: JSON.stringify(student)
+                        hash: person.hash,
+                        lastname: person.lastname,
+                        firstnames: person.firstnames,
+                        source: source,
+                        userdata: JSON.stringify(person)
                     })
             }
         }
@@ -199,7 +384,7 @@ async function updateDbForStudent(student) {
             }
         }
     } catch(err) {
-        console.log("Error processing user: " + student.sfuid)
+        console.log("Error processing user: " + person.sfuid)
         console.log(err.message)   
     }
 }
